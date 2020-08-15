4a2ffead4b1ef740aaaefce4ed3380f9860b1f12
Arrey Tabe Ebob <60329084+TABEEBOB@users.noreply.github.com>
2020-08-15 18:26:13 -0400

    Add files via upload
    I created an array to match countries and flags together

---
+++ Database.java
@@ -0,0 +1,71 @@
+package com.example.flagsandcountries;
+
+public class Database {
+
+    Integer[] flags = {
+
+            R.drawable.albania,
+            R.drawable.algeria,
+            R.drawable.america,
+            R.drawable.angola,
+            R.drawable.argentina,
+            R.drawable.australia,
+            R.drawable.austria,
+            R.drawable.bahamas,
+            R.drawable.belgium,
+            R.drawable.brazil,
+            R.drawable.cameroon,
+            R.drawable.canada,
+            R.drawable.china,
+            R.drawable.colombia,
+            R.drawable.denmark,
+            R.drawable.egypt,
+            R.drawable.finland,
+            R.drawable.france,
+            R.drawable.germany,
+            R.drawable.greece,
+            R.drawable.guatemala,
+            R.drawable.hungary,
+            R.drawable.india,
+            R.drawable.israel,
+            R.drawable.jamaica,
+            R.drawable.japan,
+            R.drawable.nigeria,
+            R.drawable.turkey,
+            R.drawable.yemen
+    };
+
+    String[] answers = {
+
+            "albania",
+            "algeria",
+            "america",
+            "angola",
+            "argentina",
+            "australia",
+            "austria",
+            "bahamas",
+            "belgium",
+            "brazil",
+            "cameroon",
+            "canada",
+            "china",
+            "colombia",
+            "denmark",
+            "egypt",
+            "finland",
+            "france",
+            "germany",
+            "greece",
+            "guatemala",
+            "hungary",
+            "india",
+            "israel",
+            "jamaica",
+            "japan",
+            "nigeria",
+            "turkey",
+            "yemen"
+    };
+    int[] scores = new int[29];
+}
