fb0094f7c6ffcc7fc10ae836ce484269ec80d73d
Arrey Tabe Ebob <60329084+TABEEBOB@users.noreply.github.com>
2020-08-15 18:18:14 -0400

    country choice
    here you find countries name and flags used.

---
+++ CountryChoice.java
@@ -0,0 +1,28 @@
+package com.example.flagsandcountries;
+
+public class CountryChoice {
+
+    public String name;
+    public int image;
+
+
+    public CountryChoice(String name, int image){
+
+        this.name = name;
+        this.image = image;
+
+    }
+
+    public  void setName(String name){
+        this.name = name;
+    }
+    public void setImage(int image){
+        this.image = image;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getImage(){
+        return image;
+    }
+}
